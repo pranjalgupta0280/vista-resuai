30d50cf8fbe0446eb4acbf6f8ea42ab535fa8001
Pranjal <versetechno007@gmail.com>
2026-04-07 10:33:41 +0530

    feat: Initialize backend with user authentication
    - Added package.json for project dependencies and scripts.
    - Created server.js to set up the Express server and connect to the database.
    - Implemented app.js to configure middleware and routes.
    - Established database connection logic in database.js.
    - Developed user authentication controllers in auth.controller.js for registration, login, logout, and fetching user details.
    - Created auth middleware for token validation in auth.middleware.js.
    - Designed user and blacklist token models in user.model.js and blacklist.model.js respectively.
    - Set up authentication routes in auth.route.js for handling user requests.

--- Backend/src/routes/auth.route.js
+++ Backend/src/routes/auth.route.js
@@ -1,8 +1,10 @@
 const express=require('express')
 const authController=require("../controllers/auth.controller")
+const authMiddleware=require("../middlewares/auth.middleware")
 const router=express.Router();
 
 router.post("/register",authController.registerUserController)
 router.post("/login",authController.loginUserController)
 router.post("/logout",authController.logoutUserController)
+router.get("/get-me",authMiddleware.authUser,authController.getMeController)
 module.exports=router; 
